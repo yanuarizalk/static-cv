a520d3c645dfcb9c2882ce82ac3a74d201f1ce5d
yanuarizalk <me@yanuarizal.net>
2022-01-20 16:35:59 +0700

    Fix non deep import declaration

--- src/javascripts/scripts.js
+++ src/javascripts/scripts.js
@@ -1,9 +1,9 @@
 import 'tailwindcss/tailwind.css';
 import { library, dom } from '@fortawesome/fontawesome-svg-core';
-import faEnvelope from '@fortawesome/free-solid-svg-icons/faEnvelope';
-import faPhone from '@fortawesome/free-solid-svg-icons/faPhone';
-import faAt from '@fortawesome/free-solid-svg-icons/faAt';
-import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons/faEnvelope';
+import { faPhone } from '@fortawesome/free-solid-svg-icons/faPhone';
+import { faAt } from '@fortawesome/free-solid-svg-icons/faAt';
+import { faGithub } from '@fortawesome/free-brands-svg-icons/faGithub';
 import Alpine from 'alpinejs';
 import axios from 'axios';
 import moment from 'moment';
