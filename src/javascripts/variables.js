f9ad3c761eab8bc938da9a4fe5d636cbca9239f1
yanuarizalk <me@yanuarizal.net>
2022-01-20 07:02:22 +0700

    Create awesome quotes of myself

--- src/javascripts/variables.js
+++ src/javascripts/variables.js
@@ -7,7 +7,7 @@ module.exports = {
   website: 'yanuarizal.net',
   address: 'Surakarta, Indonesia',
   google_place: 'Surakarta,+Surakarta+City,+Central+Java/@-7.5592034,110.7487715,12z/data=!3m1!4b1!4m5!3m4!1s0x2e7a16627ad11ab1:0xe7fe4e0454bc3095!8m2!3d-7.5754887!4d110.8243272',
-  description: 'I am a engineer',
+  description: 'I am a software engineer with wide & deep expertise in many technologies.\n With those commitment, i try to produce the best developing experience not just for consumers, but also for the developer itself.',
   educations: [
     {
       institution: 'SMK Negeri 2 Surakarta',
@@ -65,6 +65,7 @@ module.exports = {
       do: [
         'Setup network ecosystem, self hosted application like dns, mail server, sms server & etc on physical server',
         'Maintaining a CRM system to be used by company\'s employees',
+        'Attempt to deploy production ready server with load balancing traefik & grafana as monitoring system',
         'Using flutter, managed to implement realtime tracking order & chat system using firebase & rocketchat as well as on backend for their marketplace app',
         'Responsible to train interns from UDB university',
       ],
