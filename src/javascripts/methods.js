22aa717971742b966683e015cbac709cd4edf743
yanuarizalk <me@yanuarizal.net>
2022-01-21 07:58:54 +0700

    Remove unused packages & fix datefns bundle size

--- src/javascripts/methods.js
+++ src/javascripts/methods.js
@@ -1,14 +1,10 @@
-import { locale } from 'date-fns/locale/en-US/index';
-import { formatWithOptions } from 'date-fns/fp';
+// import { locale } from 'date-fns/locale/en-US/index';
+import format from 'date-fns/fp/format';
 
 export default {
   toWhatsapp: (value) => `https://wa.me/${value}`,
   mailTo: (value) => `mailto:${value}`,
   toPlace: (value) => `https://www.google.com/maps/place/${value}`,
   toGithub: (value) => `https://github.com/${value}`,
-  toDate: (fromDate, tillDate) => `${formatWithOptions({
-    locale,
-  }, 'yyyy MMM', fromDate)} - ${formatWithOptions({
-    locale,
-  }, 'yyyy MMM', tillDate)}`,
+  toDate: (fromDate, tillDate) => `${format('yyyy MMM', fromDate)} - ${format('yyyy MMM', tillDate)}`,
 };
--- src/javascripts/scripts.js
+++ src/javascripts/scripts.js
@@ -1,22 +1,16 @@
 import 'tailwindcss/tailwind.css';
-import { library, dom } from '@fortawesome/fontawesome-svg-core';
+import { library } from '@fortawesome/fontawesome-svg-core';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons/faEnvelope';
 import { faPhone } from '@fortawesome/free-solid-svg-icons/faPhone';
 import { faAt } from '@fortawesome/free-solid-svg-icons/faAt';
 import { faGithub } from '@fortawesome/free-brands-svg-icons/faGithub';
 import Alpine from 'alpinejs';
-import axios from 'axios';
 import variables from './variables';
 
 library.add(faEnvelope, faPhone, faAt, faGithub);
-dom.watch();
 
 window.Alpine = Alpine;
-window.axios = axios;
 
-/* Object.keys(variables).forEach((key) => {
-  Alpine.data(key, () => (variables[key]));
-}); */
 Alpine.data('variables', () => variables);
 
 // eslint-disable-next-line no-console
@@ -26,11 +20,5 @@ console.log(`
 
   I am only used custom css to add (.) point suffix inside list element LOL.
 `);
-
-
-/* axios.get('/partials/head.html').then((response) => {
-  console.log(response);
-});
- */
 
 Alpine.start();
